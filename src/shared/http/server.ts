027be6252528d08a31610dc3e02e815bf914ac01
Wallace B. Teixeira <wallaceb.teixeira@gmail.com>
2022-10-14 17:20:45 -0300

    chore: add product routes to router

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppErrors';
-import '@shared/typeorm'
+import '@shared/typeorm';
+import { errors } from "celebrate";
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
