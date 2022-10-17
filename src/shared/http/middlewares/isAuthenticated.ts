3ecf37f85285ccd1ea25966b2c2750ff79dc1f4b
Wallace B. Teixeira <wallaceb.teixeira@gmail.com>
2022-10-17 16:43:04 -0300

    chore: override express request object

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -18,6 +24,12 @@ export default function isAuthenticated(
 
   try {
     const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    }
 
     return next();
   } catch (error) {
