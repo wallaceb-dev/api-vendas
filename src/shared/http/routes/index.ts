5152bd9903cc0f3a12d496579601064c4b008715
Wallace B. Teixeira <wallaceb.teixeira@gmail.com>
2022-10-19 07:58:25 -0300

    chore: add routes to password recovery

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import productsRouter from 'src/modules/products/routes/products.routes';
 import sessionsRouter from 'src/modules/users/routes/sessions.routes';
 import usersRouter from 'src/modules/users/routes/users.routes';
 import { Router } from 'express';
+import passwordRouter from '@modules/users/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
