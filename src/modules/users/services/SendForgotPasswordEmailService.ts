45ebb9ef441b75e11c81f6426dd24e7500f2ed87
Wallace B. Teixeira <wallaceb.teixeira@gmail.com>
2022-10-26 19:31:01 -0300

    feat: add send mail

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/http/errors/AppErrors';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    const token = await userTokensRepository.generate(user.id);
+
+    // console.log(token);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`
+    })
+  }
+}
+
+export default SendForgotPasswordEmailService;
