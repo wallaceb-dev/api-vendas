52983d0d27332d5ff705b55367620ed4b7dd539c
Wallace B. Teixeira <wallaceb.teixeira@gmail.com>
2022-11-04 23:53:14 -0300

    chore: create orders migration

---
+++ src/shared/typeorm/migrations/1667616742313-CreateOrders.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateOrders1667616742313 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
